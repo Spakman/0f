a9f2cdfdbb0b8dc8b8d362a3508b6bd3c19647d9
Mark Somerville <my.fingers.today>
2017-12-21 16:31:42 +0000

    Make links clickable when logged in
    This involves only making <article> contenteditable when it is clicked
    on.

--- public/contenteditable.js
+++ public/contenteditable.js
@@ -1,12 +1,46 @@
 window.onload = function() {
-  let pageSaver = new PageSaver();
-
-  document.querySelectorAll("[contenteditable]").forEach(function(element) {
-    element.addEventListener("input", function(e) {
-      pageSaver.save(e.target.innerHTML);
-    });
-  });
+  new Article(document.getElementById("editableArticle"));
 };
+
+
+class Article {
+  constructor(element) {
+    this.element = element;
+    this.pageSaver = new PageSaver();
+    this.ensureLinksAreClickable();
+    this.makeElementEditable();
+  }
+
+  ensureLinksAreClickable() {
+    Array.from(this.element.getElementsByTagName("a")).forEach(function(a) {
+      a.removeEventListener("click", this.stopPropagation);
+      a.addEventListener("click", this.stopPropagation);
+    }.bind(this));
+  }
+
+  makeElementEditable() {
+    this.element.addEventListener("click", function(ev) {
+      this.contentEditable = true;
+      this.focus();
+    });
+
+    this.element.addEventListener("keyup", function(ev) {
+      if(ev.keyCode == 27) {
+        ev.target.contentEditable = false;
+        this.ensureLinksAreClickable();
+      }
+    }.bind(this));
+
+    this.element.addEventListener("input", function(ev) {
+      this.pageSaver.save(ev.target.innerHTML);
+    }.bind(this));
+  }
+
+  stopPropagation(ev) {
+    ev.stopPropagation();
+  }
+}
+
 
 class PageSaver {
   constructor() {
