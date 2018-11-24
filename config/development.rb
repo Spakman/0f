7bf3103945f40c2d34967d3938aacf77095e2efc
Mark Somerville <my.fingers.today>
2018-11-24 14:51:27 +0000

    Log exceptions in production

--- config/development.rb
+++ config/development.rb
@@ -7,6 +7,13 @@ module EnvironmentConfig
     def included(mod)
       mod.set :port, 6789
       mod.set :bind, "0.0.0.0"
+
+      error_logger = File.new("#{File.expand_path(File.dirname(__FILE__))}/../log/error.log", "a+")
+      error_logger.sync = true
+
+      mod.before {
+        env["rack.errors"] =  error_logger
+      }
     end
   end
 end
