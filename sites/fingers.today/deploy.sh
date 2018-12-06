9d04e4163b77eca6624112da0c97a30b3e026e20
Mark Somerville <my.fingers.today>
2018-12-06 09:35:03 +0000

    Remove old files on deploy

--- sites/fingers.today/deploy.sh
+++ sites/fingers.today/deploy.sh
@@ -3,20 +3,22 @@
 hostname=fingers.today
 domain=fingers.today
 base_dir=/data/0f/
-sitepath=${base_dir}/sites/${domain}/lib/
+site_base_dir=${base_dir}sites/${domain}/
+site_lib_dir=${site_base_dir}lib/
 
 if [ -z "$1" ]; then
   echo "Usage: deploy <gitobject>"
 else
   revision_for_log=$(git rev-parse $1) &&
   git archive -o deploy.tar.gz ${revision_for_log} &&
-  scp deploy.tar.gz ${hostname}:${sitepath} &&
+  scp deploy.tar.gz ${hostname}:${site_lib_dir} &&
   rm deploy.tar.gz &&
   ssh -t ${hostname} "
-    cd ${sitepath} &&
+    cd ${site_lib_dir} &&
+    find ${rootpath} ! -name deploy.tar.gz ! -name lib -print -delete &&
     tar -zxvf deploy.tar.gz &&
     rm deploy.tar.gz &&
     sudo PATH=$PATH ${base_dir%/}/lib/bin/0f --base-dir ${base_dir} --domain ${domain} --fingerprint &&
-    echo $(date): ${revision_for_log} >> deploy.log
+    echo $(date): ${revision_for_log} >> ${site_base_dir}log/deploy.log
   "
 fi
