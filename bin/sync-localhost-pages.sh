dfbf9906ccb77940d0a29ecf760e0284b58b8e51
Mark Somerville <my.fingers.today>
2019-01-31 12:39:07 +0000

    Remove bind mounts, use writable upperdir
    I had misunderstood how overlay lowerdirs work and was getting very
    strange results like files reappearing after removal.
    
    This commit uses a (writable) upperdir in place of the bind mounts and
    doesn't fall foul of the same issues.

--- bin/sync-localhost-pages.sh
+++ bin/sync-localhost-pages.sh
@@ -3,8 +3,11 @@
 domain=${1}
 deletes_file=/run/0f/${domain}/log/deletes
 
-rsync --exclude-from ${deletes_file} --delete -v -a -u -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" ${domain}@${domain}:./sites/${domain}/pages/ /data/0f/sites/${domain}/pages/ &&
+# down
+rsync --exclude-from ${deletes_file} --delete -v -a -u -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" ${domain}@${domain}:./${domain}/pages/ /run/0f/${domain}/pages/ &&
 
 echo "" > ${deletes_file} &&
 
-rsync --delete -v -a -u -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" /data/0f/sites/${domain}/pages/ ${domain}@${domain}:./sites/${domain}/pages/
+# up
+rsync --delete -v -a -u -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" /run/0f/${domain}/pages/ ${domain}@${domain}:./${domain}/pages/
+
