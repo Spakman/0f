Spakman/0f

commit b8e62dac8d9965e1ddd9968b35f92adb4ee8546e

systemd: /run/0f/ --> /data/ as per 620df2f3
Also, using hardcoded UIDs here because Arch doesn't like usernames with
dots in them and I can't get systemd to recognise the name correctly.

systemd/0f@.service
@@ -1,28 +1,20 @@
 [Unit]
 Description=%i 0f site
-Requires=0f-make-working-dir@%i.service
-After=0f-make-working-dir@%i.service
-WantedBy=nginx.service
 
 [Service]
 Type=simple
 
 Environment=GEM_HOME=/var/gem/0f/
-Environment=RACK_ENV=production
-Environment=ZEROEFF_ENV=server
+Environment=RACK_ENV=development
+Environment=ZEROEFF_ENV=localhost
 
-User=%I
-Group=%i
+User=1002
 UMask=0002
 PermissionsStartOnly=True
 
-WorkingDirectory=/run/0f/%i/
+WorkingDirectory=/data/%i/
 
-ExecStartPre=/bin/mount -t overlay overlay -o index=off,workdir=/data/0f/sites/%i/mnt/workdir-upper/,upperdir=/data/0f/sites/%i/data/,lowerdir=/data/0f/sites/%i/install/:/data/0f/sites/%i/lib/:/data/0f/lib/ /run/0f/%i/
-
-ExecStart=/usr/local/bin/ruby /run/0f/%i/0f.rb
-
-ExecStopPost=/bin/umount -l /run/0f/%i/
+ExecStart=/usr/bin/ruby /data/%i/0f.rb
 
 [Install]
 WantedBy=multi-user.target

systemd/0f-sync-localhost@.service
@@ -4,9 +4,9 @@ Description=Synchronise local data for %i 0f site
 [Service]
 Type=oneshot
 
-User=%i
+User=1002
 PermissionsStartOnly=True
 
-WorkingDirectory=/run/0f/%i/
+WorkingDirectory=/data/%i/
 
-ExecStart=/data/0f/lib/bin/sync-localhost-pages.sh
+ExecStart=/data/%i/bin/sync-localhost-pages.sh %i

commit 926505b23918e74afdd63cf950897c26ee598445

Fix broken commit b8e62dac
Mistakenly committed 0f@.service instead of creating a new file
0f-localhost@.service.

This commit reverts that part of the previous commit and adds the new
file.

bin/sync-localhost-all.sh
@@ -6,4 +6,4 @@ domain=${1}
 
 rsync -u -a -v -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" ${domain}@${domain}:./${domain}/ /data/${domain}/
 
-sudo /usr/bin/systemctl restart 0f@${domain}.service
+sudo /usr/bin/systemctl restart 0f-localhost@${domain}.service

systemd/0f-localhost@.service
@@ -1,0 +1,20 @@
+[Unit]
+Description=%i 0f site
+
+[Service]
+Type=simple
+
+Environment=GEM_HOME=/var/gem/0f/
+Environment=RACK_ENV=development
+Environment=ZEROEFF_ENV=localhost
+
+User=1002
+UMask=0002
+PermissionsStartOnly=True
+
+WorkingDirectory=/data/%i/
+
+ExecStart=/usr/bin/ruby /data/%i/0f.rb
+
+[Install]
+WantedBy=multi-user.target

systemd/0f@.service
@@ -1,20 +1,28 @@
 [Unit]
 Description=%i 0f site
+Requires=0f-make-working-dir@%i.service
+After=0f-make-working-dir@%i.service
+WantedBy=nginx.service
 
 [Service]
 Type=simple
 
 Environment=GEM_HOME=/var/gem/0f/
-Environment=RACK_ENV=development
-Environment=ZEROEFF_ENV=localhost
+Environment=RACK_ENV=production
+Environment=ZEROEFF_ENV=server
 
-User=1002
+User=%I
+Group=%i
 UMask=0002
 PermissionsStartOnly=True
 
-WorkingDirectory=/data/%i/
+WorkingDirectory=/run/0f/%i/
 
-ExecStart=/usr/bin/ruby /data/%i/0f.rb
+ExecStartPre=/bin/mount -t overlay overlay -o index=off,workdir=/data/0f/sites/%i/mnt/workdir-upper/,upperdir=/data/0f/sites/%i/data/,lowerdir=/data/0f/sites/%i/install/:/data/0f/sites/%i/lib/:/data/0f/lib/ /run/0f/%i/
+
+ExecStart=/usr/local/bin/ruby /run/0f/%i/0f.rb
+
+ExecStopPost=/bin/umount -l /run/0f/%i/
 
 [Install]
 WantedBy=multi-user.target