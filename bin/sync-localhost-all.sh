47c129ef174a134432c98a0785376476a8ae0a6e
Mark Somerville <my.fingers.today>
2019-02-14 17:18:08 +0000

    Simple script for pulling down entire site

---
+++ bin/sync-localhost-all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+domain=${1}
+
+/data/${domain}/bin/sync-localhost-pages.sh ${domain} &&
+
+rsync -u -a -v -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" ${domain}@${domain}:./${domain}/ /data/${domain}/
