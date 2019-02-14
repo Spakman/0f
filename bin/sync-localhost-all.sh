0fd2850b614ae3438c3b7b6ccfaf5d8ae2851b0e
Mark Somerville <my.fingers.today>
2019-02-14 20:08:36 +0000

    localhost: restart 0f after syncing everything

--- bin/sync-localhost-all.sh
+++ bin/sync-localhost-all.sh
@@ -5,3 +5,5 @@ domain=${1}
 /data/${domain}/bin/sync-localhost-pages.sh ${domain} &&
 
 rsync -u -a -v -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" ${domain}@${domain}:./${domain}/ /data/${domain}/
+
+sudo /usr/bin/systemctl restart 0f@${domain}.service
