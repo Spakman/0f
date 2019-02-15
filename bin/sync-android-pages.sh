b0f0e494a2daef18f2068f69db8feb72d9846cbf
Mark Somerville <my.fingers.today>
2019-02-15 23:25:26 +0000

    Fix sync so excludes aren't cleared if rsync fails

--- bin/sync-android-pages.sh
+++ bin/sync-android-pages.sh
@@ -1,12 +1,19 @@
-#!/data/data/com.termux/files/usr/bin/sh
+#!/data/data/com.termux/files/usr/bin/bash
+
+set -e
 
 domain=${1}
 excludes_file=$HOME/${domain}/log/sync-excludes
 
-# down
-rsync -i --exclude-from ${excludes_file} --delete -a -u -X -e "ssh -p3237 -i $HOME/.ssh/fingers.today_id_rsa" ${domain}@${domain}:./${domain}/pages/ $HOME/${domain}/pages/ | grep -E "^>.* " | sed -e "s@^>.* @/@" &&
+# Sync down
+#
+# Store output variable to ensure script exits if rsync fails (to avoid
+# clearing excludes).
+down_sync_output=`rsync -i --exclude-from ${excludes_file} --delete -a -u -X -e "ssh -p3237 -i $HOME/.ssh/fingers.today_id_rsa" ${domain}@${domain}:./${domain}/pages/ $HOME/${domain}/pages/`
 
-echo "" > ${excludes_file} &&
+echo "${down_sync_output}" | grep -E "^>.* " | sed -e "s@^>.* @/@"
 
-# up
+echo "" > ${excludes_file}
+
+# Sync up
 rsync --delete -a -u -X -e "ssh -p3237 -i $HOME/.ssh/fingers.today_id_rsa" $HOME/${domain}/pages/ ${domain}@${domain}:./${domain}/pages/
--- bin/sync-localhost-pages.sh
+++ bin/sync-localhost-pages.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/usr/bin/bash
+
+set -e
 
 domain=${1}
 excludes_file=/data/${domain}/log/sync-excludes
 
-# down
-rsync -i --exclude-from /data/fingers.today/log/sync-excludes --delete -a -u -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" fingers.today@fingers.today:./fingers.today/pages/ /data/fingers.today/pages/ | grep -E "^>.* " | sed -e "s@^>.* @/@" &&
+# Sync down
+#
+# Store output variable to ensure script exits if rsync fails (to avoid
+# clearing excludes).
+down_sync_output=`rsync -i --exclude-from /data/fingers.today/log/sync-excludes --delete -a -u -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" fingers.today@fingers.today:./fingers.today/pages/ /data/fingers.today/pages/`
 
-echo "" > ${excludes_file} &&
+echo "${down_sync_output}" | grep -E "^>.* " | sed -e "s@^>.* @/@"
 
-# up
+echo "" > ${excludes_file}
+
+# Sync up
 rsync --delete -a -u -X -A -e "ssh -p3237 -i $HOME/.ssh/id_rsa" /data/${domain}/pages/ ${domain}@${domain}:./${domain}/pages/
