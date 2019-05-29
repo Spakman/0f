502e22eb18fb88df090e2a7153d18299592fd65b
Mark Somerville <my.fingers.today>
2019-05-29 21:19:57 +0100

    Additionally authenticate with client CN
    When there was only one site and user to worry about, checking for a
    valid certificate was enough.

--- sites/fingers.today/config/server.rb
+++ sites/fingers.today/config/server.rb
@@ -1,14 +1,17 @@
 module EnvironmentConfig
   def authenticated?
-    request.env["HTTP_X_CLIENT_AUTHENTICATED"] == "SUCCESS" && (
-      request.env["HTTP_X_CLIENT_COMMON_NAME"] =~ /CN=continuingadventures.xyz/ ||
-        request.env["HTTP_X_CLIENT_COMMON_NAME"] =~ /CN=fingers.today/
-    )
+    warn(:res)
+    warn(request.env["HTTP_X_CLIENT_AUTHENTICATED"] == "SUCCESS" &&
+      request.env["HTTP_X_CLIENT_COMMON_NAME"] =~ /CN=fingers.today/)
+    warn(:res)
+
+    request.env["HTTP_X_CLIENT_AUTHENTICATED"] == "SUCCESS" &&
+      request.env["HTTP_X_CLIENT_COMMON_NAME"] =~ /CN=fingers.today/
   end
 
   class << self
     def included(mod)
-      mod.set :port, 7890
+      mod.set :port, 6789
 
       error_logger = File.new("#{File.expand_path(File.dirname(__FILE__))}/../log/error.log", "a+")
       error_logger.sync = true
